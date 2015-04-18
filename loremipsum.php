2b7d7c84d20b33186f92f3a0cf3cf235440d6852
Semyon Maryasin <simeon@maryasin.name>
2015-04-18 19:20:06 +0300

    constructor (part)

--- loremipsum.php
+++ loremipsum.php
@@ -11,4 +11,21 @@ if (!defined('_PS_VERSION_'))
 
 class LoremIpsum extends Models
 {
+	public function __construct()
+	{
+		$this->name = 'loremipsum';
+		$this->tab = 'content_management';
+		$this->version = '1.0.0';
+		$this->author = 'Semyon Maryasin';
+		$this->need_instance = 0;
+		$this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+		$this->bootstram = true;
+
+		parent::__construct();
+
+		$this->displayName = $this->l('LoremIpsum');
+		$this->description = $this->l('Fill empty product descriptions with generated «Lorem Ipsum» text');
+
+		//
+	}
 }
