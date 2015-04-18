cbd1f08a5f87c8d3a2508308e25683d3cd46b12b
Semyon Maryasin <simeon@maryasin.name>
2015-04-18 19:15:19 +0300

    added main file

---
+++ loremipsum.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* This module fills empty goods descriptions with «Lorem Ipsum» text.
+*
+* @author  Semyon Maryasin <simeon@maryasin.name>
+* @license GPL
+*/
+
+if (!defined('_PS_VERSION_'))
+	exit;
+
+class LoremIpsum extends Models
+{
+}
